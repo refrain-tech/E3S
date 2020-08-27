9e747e4b2eaa11fc31cff66842af34441a96c397
Refrain Tech <59395606+refrain-tech@users.noreply.github.com>
2020-08-27 13:57:31 +0900

    Update and rename script.js to e3s-core@1.0.js

--- e3s-core@1.0.js
+++ e3s-core@1.0.js
@@ -1,21 +1,31 @@
+/**
+ * @overview e3s-core@1.0.js E3Sの制御プログラム
+ * @author Refrain <refrain.tech@gmail.com>
+ * @since 2020/8/27
+ * @copyright (c) Copyright 2020 Refrain All Rights Reserved.
+ */
 'use strict';
-const pickImmobileList = document.querySelector('#pickImmobileList'),
-      pickHolidayList = document.querySelector('#pickHolidayList'),
-      inputStart = document.querySelector('#inputStart'),
-      inputFinish = document.querySelector('#inputFinish'),
-      inputSpan = document.querySelector('#inputSpan'),
-      clickImmobileListPicker = document.querySelector('#clickImmobileListPicker'),
-      clickHolidayListPicker = document.querySelector('#clickHolidayListPicker'),
-      inputYear = document.querySelector('#inputYear'),
-      inputMonth = document.querySelector('#inputMonth'),
-      inputDate = document.querySelector('#inputDate'),
-      inputHour = document.querySelector('#inputHour'),
-      inputMinute = document.querySelector('#inputMinute'),
-      runButton = document.querySelector('#runButton'),
-      copyButton = document.querySelector('#copyButton'),
-      resultTable = document.querySelector('#resultTable');
+/** @type {HTMLElement} GUI部品を取得する */
+// const pickImmobileList = document.querySelector('#pickImmobileList');
+// const pickHolidayList = document.querySelector('#pickHolidayList');
+const inputStart = document.querySelector('#inputStart');
+const inputFinish = document.querySelector('#inputFinish');
+const inputSpan = document.querySelector('#inputSpan');
+// const clickImmobileListPicker = document.querySelector('#clickImmobileListPicker');
+// const clickHolidayListPicker = document.querySelector('#clickHolidayListPicker');
+const inputYear = document.querySelector('#inputYear');
+const inputMonth = document.querySelector('#inputMonth');
+const inputDate = document.querySelector('#inputDate');
+const inputHour = document.querySelector('#inputHour');
+const inputMinute = document.querySelector('#inputMinute');
+const runButton = document.querySelector('#runButton');
+const copyButton = document.querySelector('#copyButton');
+const resultTable = document.querySelector('#resultTable');
+/** @type {Array<Number | String>} 走査中に得られたデータの保存先 */
 const CACHE_DATA = [ ];
+/** @type {RegExp} フォーマット確認用の正規表現 */
 const IM_PATTERN = /\d{4}\/\d{2}\/\d{2}\s\d{2}:\d{2}~\d{4}\/\d{2}\/\d{2}\s\d{2}:\d{2}_.+/;
+/** @type {RegExp} フォーマット確認用の正規表現 */
 const HO_PATTERN = /\d{4}\/\d{2}\/\d{2}/;
 /** @type {Number} タスクの開始時点の時間(ミリ秒) */
 let base;
@@ -34,9 +44,9 @@ let temp;
 /** @type {Number} タスクの経過時間 */
 let total;
 /** @type {Array<String>} 試験ができない(= 設備が動いていない)期間のリスト */
-let imList = [ ];
+let imList;
 /** @type {Array<String>} 対応ができない(= 設備が動いている)日付のリスト */
-let hoList = [ ];
+let hoList;
 /** @summary イベントハンドラの登録 */
 // pickImmobileList.addEventListener('change', onChange, false);
 // pickHolidayList.addEventListener('change', onChange, false);
@@ -93,17 +103,19 @@ function onClick (event) {
   }
 }
 /**
- * @function onClick 変数の初期化
+ * @function onClick 全データの初期化処理
  */
 function initialize () {
   start = parseInt(inputStart.value);
-  finish = parseInt(inputFinish.value);
   span = parseInt(inputSpan.value);
   total = 0;
-  loop = Math.floor(start/span);
-  dis = 0;
-  temp = new Date(`${inputYear.value}/${inputMonth.value}/${inputDate.value} ${inputHour.value}:${inputMinute.value}`);
   base = temp.getTime();
+  dis = 0;
+  finish = parseInt(inputFinish.value);
+  loop = (start / span) | 0;
+  temp = new Date(`${inputYear.value}/${inputMonth.value}/${inputDate.value} ${inputHour.value}:${inputMinute.value}`);
+  imList = [ ];
+  hoList = [ ];
   CACHE_DATA.length = 0;
   resultTable.querySelector('tbody').innerHTML = '';
 }
@@ -147,27 +159,42 @@ function parse (millisecond) {
  * @return {Boolean} 参照日時がimListに含まれていたか
  */
 function checkImmobile () {
-  let flag = false, stop, restart, reason;
-  imList.forEach(currentValue => {
-    [ stop, restart, reason ] = currentValue;
+  let stop, restart, reason;
+  // imListを走査する
+  for (const item of imList) {
+    // 停止させる日時、再開させる日時、停止させる理由を取得する
+    [ stop, restart, reason ] = item;
+    // 参照中の日付が停止させる日付と一致した場合
     if (format()[0] === stop.split(' ')[0]) {
+      // 停止させる日時からDateオブジェクトを生成する
       const stopDate = new Date(stop);
+      // データを保存
       save(stopDate);
+      // 経過時間の総和を更新する
+      // 経過時間の総和 = 停止させた時間 - 最初の開始時間 - 7(10時から17時までの7時間) x 実施回数 - 停止させる時間 + 開始時点で経過している時間
       total = parse(stopDate.getTime() - base) - 7 * loop - dis + start;
+      // 再開させる日時からDateオブジェクトを生成する
       temp = new Date(restart);
+      // 停止させていた時間を加算する
       dis += parse(temp.getTime() - stopDate.getTime());
-      flag = true;
+      // データを保存
       CACHE_DATA.push(total);
+      // データを保存
       CACHE_DATA.push(reason);
+      // データを吐き出す
       output();
+      // 経過時間の総和が実施回数 x タスク毎の時間を超えていた場合、回数を+1する
+      // t時間で停止させるのに2t時間が経過していた場合、ループ回数の修正が必要
       if (total >= (loop + 1) * span) loop ++;
+      // 走査を終了する
+      return true;
     }
-  });
-  return flag;
+  }
+  return false;
 }
 /**
  * @function format 参照中のDateオブジェクトを任意の形式に変換する
- * @return {Array<String>} 指定形式([ YYYY/MM/DD, hh:mm ])に変換された日時
+ * @return {Array<String>} 指定形式([ YYYY/MM/DD, hh:mm ])に変換されたDateオブジェクト
  */
 function format () {
   return [
@@ -188,12 +215,15 @@ function save (date = temp) {
 function output () {
   const tr = document.createElement('tr');
   let td;
+  // 全データを<td>要素として出力する
   CACHE_DATA.forEach(currentValue => {
     td = document.createElement('td');
     td.textContent = currentValue;
     tr.appendChild(td);
   });
+  // テーブル上に出力する
   resultTable.querySelector('tbody').appendChild(tr);
+  // データの破棄
   CACHE_DATA.length = 0;
 }
 /**
