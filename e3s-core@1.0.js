533d3b72225eaf689f8f00877fa36451776f41b0
Refrain Tech <59395606+refrain-tech@users.noreply.github.com>
2020-08-31 16:33:46 +0900

    Update e3s-core@1.0.js

--- e3s-core@1.0.js
+++ e3s-core@1.0.js
@@ -1,56 +1,50 @@
 /**
  * @overview e3s-core@1.0.js E3Sの制御プログラム
  * @author Refrain <refrain.tech@gmail.com>
- * @since 2020/8/27
+ * @since 2020/9/1
  * @version 1.0
  * @copyright (c) Copyright 2020 Refrain All Rights Reserved.
  */
 'use strict';
-/** @type {Array<Number | String>} 走査中に得られたデータの保存先 */
+/** @type {Array<Number|String>} 走査中に得られたデータの保存先 */
 const CACHE_DATA = [ ];
-/** @type {Array<String>} 試験ができない(= 設備が動いていない)期間のリスト */
+/** @type {Array<String>} 設備が動いていない期間のリスト */
 const IM_LIST = [ ];
-/** @type {Array<String>} 対応ができない(= 設備が動いている)日付のリスト */
+/** @type {Array<String>} 休日のリスト */
 const HO_LIST = [ ];
 /** @type {RegExp} フォーマット確認用の正規表現 */
 const IM_PATTERN = /^\d{4}\/\d{2}\/\d{2}\s\d{2}:\d{2}~\d{4}\/\d{2}\/\d{2}\s\d{2}:\d{2}_.+$/;
 /** @type {RegExp} フォーマット確認用の正規表現 */
 const HO_PATTERN = /^\d{4}\/\d{2}\/\d{2}$/;
-/** @type {Date} タスクの開始時点のDateオブジェクト */
+/** @type {Date} 試験開始時点のDateオブジェクト */
 let baseDate;
-/** @type {Number} タスクが実施されていない時間の総和(誤差補正に使用する) */
+/** @type {Number} 設備が停止している時間 */
 let disableTime;
-/** @type {Number} タスクが終了する上限値 */
+/** @type {Number} 試験が終了する上限値 */
 let limit;
-/** @type {Number} タスクの行われた回数(誤差補正に使用する) */
+/** @type {Number} 取り出し回数 */
 let loop;
-/** @type {Number} タスク毎の時間 */
+/** @type {Number} ステップ毎の時間 */
 let span;
-/** @type {Number} タスクの開始時点での経過時間 */
+/** @type {Number} 試験開始時点での経過時間 */
 let base;
 /** @type {Date} 参照を続けるDateオブジェクト */
 let refDate;
-/** @type {Number} タスクの経過時間 */
+/** @type {Number} 試験の経過時間 */
 let total;
 /**
  * @function loadConfig コンフィグファイルの読み込み
  * @argument {File} file 読み込み対象のファイル
  */
 function loadConfig (file) {
+  /** @summary データの初期化処理 */
   IM_LIST.length = 0;
   HO_LIST.length = 0;
   readFile(file).then(result => {
     result.replace(/\r/g, '').split('\n').forEach(value => {
-      switch (true) {
-        case IM_PATTERN.test(value):
-          IM_LIST.push(value.split(/[~_]/));
-          break;
-        case HO_PATTERN.test(value):
-          HO_LIST.push(value);
-          break;
-        default:
-          break;
-      }
+      /** @summary 正規表現で検査し、保存先を変更する */
+      if (IM_PATTERN.test(value)) IM_LIST.push(value.split(/[~_]/));
+      else if (HO_PATTERN.test(value)) HO_LIST.push(value);
     });
     window.alert('読み込みが完了しました。');
   }).catch(error => {
@@ -66,8 +60,10 @@ function loadConfig (file) {
 function readFile (file) {
   return new Promise((resolve, reject) => {
     const fileReader = new FileReader();
+    /** @summary イベント発生時にresolve/rejectを呼び出す */
     fileReader.addEventListener('error', event => reject(fileReader.error), false);
     fileReader.addEventListener('load', event => resolve(fileReader.result), false);
+    /** @summary UTF-8形式で読み込む */
     fileReader.readAsText(file, 'utf-8');
   });
 }
@@ -80,38 +76,43 @@ function init () {
   limit = parseInt(UJNWVR0g.value);
   span = parseInt(ZHgPpUJS.value);
   loop = (base / span) | 0;
-  disableTime = -7 * loop; // 開始時点で蓄積される停止時間を相殺する
+  /** @summary 開始時点で蓄積される停止時間を相殺する */
+  disableTime = -7 * loop;
   baseDate = new Date(`${iophZzyF.value}/${GFZYmEFU.value}/${QR0Oq3bL.value} ${az1m1nnB.value}:${NMQr9RMs.value}`);
-  refDate = new Date(baseDate); // オブジェクトは参照渡しになるので、baseDateを引数に作成する
+  /** @summary オブジェクトは参照渡しになるので、baseDateを引数に作成する */
+  refDate = new Date(baseDate);
   CACHE_DATA.length = 0;
   YR6JWQam.querySelector('tbody').innerHTML = '';
 }
 /**
  * @function main 日程演算のメイン部
  * @description 1. refDateのデータを保存する
- *              2. 10時に設定する(標準的な停止時刻)
- *              3. 経過時間を更新し、経過時間 < ループ回数 x スパンか、休日判定であればrefDateを翌日に変更する
- *              4. 設備を停止させる日なら、trueを返して関数を抜け、親ループを継続する
- *              5. refDateのデータを保存する
- *              6. 経過時間と停止理由(停止させないので'')を保存する
- *              7. データをテーブルに出力する
- *              8. ループ回数を+1する
- *              9. 17時に設定する(標準的な再開時刻)
- *              10. 経過時間が上限に達しているかを返し、親ループを制御する
+ *              2. 0分に修正する
+ *              3. 10時に設定する
+ *              4. 経過時間を更新し、経過時間 < ループ回数 x スパンか、休日を検出する
+ *              5. refDateを翌日に変更する
+ *              6. 設備を停止させる日なら、trueを返して関数を抜け、親ループを継続する
+ *              7. refDateのデータを保存する
+ *              8. 経過時間と停止理由(停止させないので'')を保存する
+ *              9. データをテーブルに出力する
+ *              10. ループ回数を+1する
+ *              11. 17時に設定する
+ *              12. 経過時間が上限に達しているかを返し、親ループを制御する
  */
 function main () {
   toCache();                                                           // 1
-  setDate(10);                                                         // 2
-  while ((total = getTotal()) < span * (loop + 1) || checkHoliday()) { // 3
-    refDate.setDate(refDate.getDate() + 1);
-    if (checkImmobile()) return true;                                  // 4
+  refDate.setMinutes(0);                                               // 2
+  setDate(10);                                                         // 3
+  while ((total = getTotal()) < span * (loop + 1) || checkHoliday()) { // 4
+    refDate.setDate(refDate.getDate() + 1);                            // 5
+    if (checkImmobile()) return true;                                  // 6
   }
-  toCache();                                                           // 5
-  CACHE_DATA.push(total, total - total % span);                        // 6
-  arr2table();                                                         // 7
-  loop ++;                                                             // 8
-  setDate(17);                                                         // 9
-  return total < limit;                                                // 10
+  toCache();                                                           // 7
+  CACHE_DATA.push(total, total - total % span);                        // 8
+  arr2table();                                                         // 9
+  loop ++;                                                             // 10
+  setDate(17);                                                         // 11
+  return total < limit;                                                // 12
 }
 /**
  * @function toCache refDateの各値を保存する
@@ -120,11 +121,13 @@ function toCache () {
   CACHE_DATA.push(refDate.getFullYear(), refDate.getMonth() + 1, refDate.getDate(), refDate.getHours(), refDate.getMinutes());
 }
 /**
- * @function setDate refDateを任意の時間に設定する(指定時間が参照時間より前なら、翌日に補正する)
- * @argument {Number} hour 設定する時間(0 ~ 23に変換される)
+ * @function setDate refDateを任意の時間に設定する
+ * @argument {Number} hour 設定する時間
  */
 function setDate (hour) {
+  /** @summary 24で割った余りを出し、0～23に丸め込む */
   hour %= 24;
+  /** @summary 指定時間が参照時間より前なら、翌日に補正する */
   if (refDate.getHours() > hour) refDate.setDate(refDate.getDate() + 1);
   refDate.setHours(hour);
 }
@@ -156,16 +159,17 @@ function checkHoliday () {
  * @return {Boolean} 参照日時がimListに含まれていたか
  * @description 1. imListをfor...ofでループする
  *              2. 得られたデータをそれぞれの変数に代入する
- *              3. refDateの日付と停止日が一致しなければ、ループを再開する
- *              4. 停止日をDateオブジェクトにする
+ *              3. refDateの日付と停止日が一致しなければループを再開する
+ *              4. 停止日でDateオブジェクトを作成する
  *              5. stopDateのデータを保存する
  *              6. 経過時間を更新する
  *              7. 経過時間と停止理由を保存する
  *              8. データをテーブルに出力する
- *              9. refDateを再開日にする
- *              10. refDate - stopDateで停止させていた時間を取得する
- *              11. trueを返し関数を終了し、親ループを終了する
- *              12. falseを返し関数を終了し、親ループを継続する
+ *              9. 再開日でDateオブジェクトを作成する
+ *              10. 再開日 - 停止日で停止させていた時間を取得する
+ *              11. refDateを更新する
+ *              12. trueを返し関数を終了し、親ループを終了する
+ *              13. falseを返し関数を終了し、親ループを継続する
  */
 function checkImmobile () {
   let stop, restart, reason;
@@ -179,10 +183,10 @@ function checkImmobile () {
     arr2table();                                              // 8
     const temp = new Date(restart);                           // 9
     disableTime += ms2hr(temp.getTime() - refDate.getTime()); // 10
-    refDate = temp;
-    return true;                                              // 11
+    refDate = temp;                                           // 11
+    return true;                                              // 12
   }
-  return false;                                               // 12
+  return false;                                               // 13
 }
 /**
  * @function format 参照中のDateオブジェクトを任意の形式に変換する
